c63da9bafc4a7af0c90390923af7c3d5ff566ac6
Pascal Berrang <mail@paberr.net>
2018-08-30 12:22:09 +0200

    Update package and build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,36 @@
 // rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+
 export default [
     {
         input: 'build/main.js',
         output: {
-            file: 'dist/boruca.common.js',
+            file: 'dist/rpc-events.common.js',
             format: 'cjs'
-        }
+        },
+        external: '@nimiq/rpc'
     },
     {
         input: 'build/main.js',
         output: {
-            file: 'dist/boruca.umd.js',
+            file: 'dist/rpc-events.umd.js',
             format: 'umd',
-            name: 'Boruca',
+            name: 'Rpc',
+            extend: true,
             globals: {
                 '@nimiq/rpc': 'Rpc'
             }
-        }
+        },
+        external: '@nimiq/rpc'
+    },
+    {
+        input: 'build/main.js',
+        output: {
+            file: 'dist/rpc-events.es.js',
+            format: 'es'
+        },
+        plugins: [
+            resolve()
+        ]
     }
 ];
